67133180442827b5671beeb1c3e3f4a0a0310384
BenjaminPasco <benjamin.pasco@hotmail.fr>
2019-09-13 18:49:45 +0200

    End Session 2
    End Session 2

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -6,16 +6,20 @@ import {
   MatInputModule,
   MatCardModule,
   MatButtonModule,
-  MatToolbarModule } from '@angular/material';
+  MatToolbarModule,
+  MatExpansionModule
+ } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './post-create/post-create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    PostListComponent,
     HeaderComponent
   ],
   imports: [
@@ -25,7 +29,8 @@ import { HeaderComponent} from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
