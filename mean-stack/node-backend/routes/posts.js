8b8eeb7a63d161b99094d6c7542c9e2c9b35c7c9
BenjaminPasco <39671385+BenjaminPasco@users.noreply.github.com>
2019-12-25 23:00:52 +0100

    paginator

--- mean-stack/node-backend/routes/posts.js
+++ mean-stack/node-backend/routes/posts.js
@@ -65,11 +65,25 @@ router.put('/:id', multer({storage: storage}).single('image'),(req, res, next) =
 })
 
 router.get('/',(req, res, next) => {
-  Post.find()
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.currentpage;
+  const postQuery = Post.find();
+  let fetchedPosts;
+  if (pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery
     .then(documents => {
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then( count => {
       res.status(200).json({
         message: 'posts fetched',
-        posts: documents
+        posts: fetchedPosts,
+        maxPosts: count
       });
     })
     .catch();
