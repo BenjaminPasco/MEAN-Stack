2ad481167d45bd91b960c076a08e9de46b654ad8
BenjaminPasco <39671385+BenjaminPasco@users.noreply.github.com>
2019-12-27 01:51:57 +0100

    12 fini

--- mean-stack/node-backend/app.js
+++ mean-stack/node-backend/app.js
@@ -6,27 +6,29 @@ const path = require('path');
 const app = express();
 
 const postsRoutes = require('./routes/posts');
+const usersRoutes = require('./routes/users');
 
 app.use((bodyParser.json()));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/images', express.static(path.join('node-backend/images')));
 
 mongoose.connect('mongodb+srv://benjaminNoAdmin:1234@cluster0-1e4ks.mongodb.net/node-angular?retryWrites=true&w=majority')
-.then(() => {
-  console.log('Connected to db');
-})
-.catch(() => {
-  console.log('Connection Failed');
-});
+  .then(() => {
+    console.log('Connected to db');
+  })
+  .catch(() => {
+    console.log('Connection Failed');
+  });
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE, PUT, OPTIONS');
   next();
 });
 
 app.use('/api/posts',postsRoutes);
+app.use('/api/users',usersRoutes);
 
 
 
--- mean-stack/node-backend/routes/posts.js
+++ mean-stack/node-backend/routes/posts.js
@@ -3,6 +3,9 @@ const multer = require('multer');
 
 const router = express.Router();
 
+const Post = require('../models/post');
+const checkAuth = require('../middlewares/check-auth');
+
 const MIME_TYPE_MAP = {
   'image/png': 'png',
   'image/jpeg': 'jpg',
@@ -25,9 +28,7 @@ const storage = multer.diskStorage({
   }
 });
 
-const Post = require('../models/post');
-
-router.post('/', multer({storage: storage}).single('image'),(req, res, next) => {
+router.post('/', checkAuth, multer({storage: storage}).single('image'),(req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const  post = new Post({
     title: req.body.title,
@@ -47,7 +48,7 @@ router.post('/', multer({storage: storage}).single('image'),(req, res, next) => 
   });
 });
 
-router.put('/:id', multer({storage: storage}).single('image'),(req, res, next) => {
+router.put('/:id', checkAuth, multer({storage: storage}).single('image'),(req, res, next) => {
   let imagePath = req.body.imagePath;
   if(req.file) {
     const url = req.protocol + '://' + req.get('host');
@@ -99,7 +100,7 @@ router.get('/:id',(req, res, next) => {
   })
 })
 
-router.delete('/:id',(req, res, next) => {
+router.delete('/:id', checkAuth,(req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
     res.status(200).json({ message: 'Post deleted'});
   })
